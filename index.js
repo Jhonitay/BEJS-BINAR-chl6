ab717ec096de6bd5919fb88e5d9dbbae7460f1f0
jhonitay <jhonitay06@gmail.com>
2023-11-01 21:26:27 +0700

    memperbaiki env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json({strict : false}))
 app.use(cors())
 app.use('/v1', router)
 
-app.get('v2', (req, res) => {
+app.get('/v2', (req, res) => {
     return res.status(200).json({
         msg : 'cek status cuy'
     })
