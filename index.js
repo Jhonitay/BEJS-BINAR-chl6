2252ce37e4c1221a00ab8de9e41d8cdf8bb4cf9f
jhonitay <jhonitay06@gmail.com>
2023-11-01 21:16:47 +0700

    memperbaiki env

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.json({strict : false}))
 app.use(cors())
 app.use('/v1', router)
 
+app.get('v2', (req, res) => {
+    return res.status(200).json({
+        msg : 'cek status cuy'
+    })
+})
+
 app.get('*', (req, res) => {
     return res.status(404).json({
         error : 'endPoint tidak ditemukan'
